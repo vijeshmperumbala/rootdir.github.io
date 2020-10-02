e502c5de267f22a0ff534bd52a79f3bda5c7ea15
vijesh m <vijeshmperumbala31@gmail.com>
2020-10-02 17:48:16 +0530

    first commit test fully developed web

---
+++ assets/js/script.js
@@ -0,0 +1,140 @@
+$(document).ready(function() {
+    var w = window.innerWidth;
+
+    if (w > 767) {
+        $('#menu-jk').scrollToFixed();
+    } else {
+        // $('#menu-jk').scrollToFixed();
+    }
+
+
+
+})
+
+
+$(document).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 5000,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+
+
+});
+
+// slide-2
+$(document).ready(function() {
+    $('.card-slid').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+});
+
+// read more
+$(document).ready(function() {
+    function AddReadMore() {
+        var carLmt = 150;
+        var readMoreTxt = " .... Read More";
+        var readLessTxt = " Read Less";
+
+        $(".addReadMore").each(function() {
+            if ($(this).find(".firstSec").length)
+                return;
+
+            var allstr = $(this).text();
+            if (allstr.length > carLmt) {
+                var firstSet = allstr.substring(0, carLmt);
+                var secdHalf = allstr.substring(carLmt, allstr.length);
+                var strtoadd = firstSet + "<span class='SecSec'>" + secdHalf + "</span><span class='readMore'  title='Click to Show More'>" + readMoreTxt + "</span><span class='readLess' title='Click to Show Less'>" + readLessTxt + "</span>";
+                $(this).html(strtoadd);
+            }
+
+        });
+        $(document).on("click", ".readMore,.readLess", function() {
+            $(this).closest(".addReadMore").toggleClass("showlesscontent showmorecontent");
+        });
+    }
+    $(function() {
+        AddReadMore();
+    });
+});
+
+// load more
+$(document).ready(function() {
+    $(".moreBox").slice(0, 3).show();
+    if ($(".blogBox:hidden").length != 0) {
+        $("#loadMore").show();
+    }
+    $("#loadMore").on('click', function(e) {
+        e.preventDefault();
+        $(".moreBox:hidden").slice(0, 3).slideDown();
+        if ($(".moreBox:hidden").length == 0) {
+            $("#loadMore").fadeOut('slow');
+        }
+    });
+});
+
+
+
+
+$(document).ready(function() {
+
+    $(".filter-button").click(function() {
+        var value = $(this).attr('data-filter');
+
+        if (value == "all") {
+
+            $('.filter').show('1000');
+        } else {
+
+            $(".filter").not('.' + value).hide('1000');
+            $('.filter').filter('.' + value).show('1000');
+
+        }
+    });
+
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+
+});
